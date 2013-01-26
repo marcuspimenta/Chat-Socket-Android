79c70d53ea6ce6c98ce38f3d62b16ce70288530e
marcuspimenta <mvinicius.pimenta@gmail.com>
2013-01-26 07:50:28 -0300

    Configuracao do timeout do serversocket

--- Socket/src/com/socket/manager/SocketServer.java
+++ Socket/src/com/socket/manager/SocketServer.java
@@ -12,11 +12,15 @@ import java.net.Socket;
  */
 public class SocketServer {
 	
+	private final int TIME_OUT = 15000;
+	
 	private Socket socket;
 	
 	public Socket startServer(int port){
 		try {
 			ServerSocket server = new ServerSocket(port);
+			server.setSoTimeout(TIME_OUT);
+			
 			socket = server.accept();
 			
 			return socket;
